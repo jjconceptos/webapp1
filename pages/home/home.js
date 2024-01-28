3fd3230f2239250f213310261feec20a32007ed0
jjconceptos <jjconceptos21@gmail.com>
2024-01-27 18:13:35 -0600

    initial

--- pages/home/home.js
+++ pages/home/home.js
@@ -60,6 +60,88 @@ const Home = () => {
           font-size: 32px;
           white-space: nowrap; /* Prevent wrapping to the next line */
         }
+
+         @media only screen and (max-width: 600px) {
+          ul {
+           
+            display: flex;
+            
+            flex-wrap: wrap; /* Ensure flex items wrap on smaller screens */
+            align-items: center;
+            margin: 0;
+            padding: 0;
+            position: static;
+            margin-top: 10px;
+            margin-left: 5px;
+            z-index: 2; // Ensure it appears above other elements
+            font-size: 12px;
+          }
+        
+          ul li {
+            margin: 5px; /* Adjusted margin for more compact spacing */
+          }
+        
+          footer {
+            position: fixed;
+            bottom: 0;
+            left: 0;
+            width: 100%;
+            height: 8vh;
+            background-color: rgba(52, 73, 94);
+            padding: 10px;
+            color: black;
+            font-size: 1vw; /* Adjusted for responsiveness */
+            opacity: 0.8;
+            z-index: 2;
+            display: flex;
+            flex-direction: row; /* Arrange items in a row */
+            align-items: center; /* Align items vertically in the middle */
+            justify-content: space-between; /* Add space between items */
+          }
+        
+          .footer-section {
+            font-size: 1.5vw; /* Adjusted for responsiveness */
+            margin-right: 20px; /* Add some space between sections */
+            display: flex;
+            flex-direction: column; /* Arrange items in a column inside each section */
+          }
+        
+          .footer-copyright {
+            font-size: 2vw; /* Adjusted for responsiveness */
+            margin-top: 2%;
+            margin-right: 1%;
+          }
+        }
+
+        @media only screen and (max-width: 600px) {
+          .type-in-text {
+            position: fixed; /* Fix the position */
+            width: 100%; /* Take the full width of the viewport */
+            top: 50%; /* Center vertically at 50% from the top */
+            transform: translateY(-50%); /* Adjust for vertical centering */
+            text-align: center; /* Center the text horizontally within the container */
+          }
+        
+          .type-in-text h1 {
+            font-size: 28px;
+            white-space: nowrap;
+            position: relative; /* Change position to relative */
+            left: 0%; /* Move the text 50% to the right */
+            transform: translateX(-50%); /* Adjust for horizontal centering */
+          }
+        
+        }
+
+/* Media query for screens between 769px and 1024px */
+@media only screen and (min-width: 769px) and (max-width: 1024px) {
+  ul {
+    /* Add specific styles for this screen size */
+  }
+
+  footer {
+    /* Add specific styles for this screen size */
+  }
+}
       `}</style>
 
       {/* Content above the image */}
