ab8bc57c3b2b53127e45b66bcf734a9ee6932fdf
jjconceptos <jjconceptos21@gmail.com>
2024-01-29 22:14:17 -0600

    initial

--- pages/projects/furniture/projects.js
+++ pages/projects/furniture/projects.js
@@ -199,6 +199,113 @@ function FurnitureProjects() {
         .close-button:hover {
           color: lightgray; /* Adjust hover color as needed */
         }
+
+        @media only screen and (max-width: 600px) {
+
+          .no-projects-centered {
+            position: fixed;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+          }
+  
+          .add-projects {
+            position: absolute;
+            top: 16%;
+            left: 25%;
+            transform: translate(-50%, -50%);
+          }
+  
+          .enlarged-view {
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(0, 0, 0, 0.94); /* Adjust transparency as needed */
+            display: flex;
+            z-index: 100; /* Higher z-index than other elements */
+          }
+  
+          
+          .enlarged-container {
+            max-width: 80%; /* Adjust as needed */
+            max-height: 80vh; /* Adjust as needed */
+            overflow: hidden;
+          }
+  
+          .enlarged-project {
+            width: 100%;
+            height: 100%;
+            display: flex;
+            flex-direction: column;
+            align-items: flex-start; /* Align to the left */
+            justify-content: center;
+          }
+          
+          .enlarged-project-img {
+            width: 80%; 
+            max-width: 100%;
+            height: auto;
+            max-height: 80vh; 
+            object-fit: contain;
+            margin-left: 15%;
+            margin-top: 10%;
+          }
+  
+          .project-name {
+           
+          }
+        
+          .project-description {
+           
+          }
+  
+          .enlarged-project-name {
+            font-size: 3vw; /* Adjust font size as needed */
+            margin-left: 20%; /* Adjust margin-left as needed */
+            margin-top: 5%; /* Adjust margin-top as needed */
+            color: #f3f0e9;
+          }
+        
+          .enlarged-project-description {
+            font-size: 2vw;
+            margin-left: 20%;
+            margin-top: 2%;
+            color: #f3f0e9;
+            white-space: pre-line; /* Preserve newlines and spaces */
+            overflow-wrap: break-word; /* Wrap long words */
+            max-width: 60%; /* Adjust as needed */
+          }
+            
+          .close-button {
+            position: absolute;
+            top: 10px;
+            right: 10px;
+            background: none;
+            border: none;
+            font-size: 24px;
+            cursor: pointer;
+            color: white; /* Adjust as needed */
+            outline: none;
+            transition: color 0.3s ease;
+          }
+          
+          .close-button:hover {
+            color: lightgray; /* Adjust hover color as needed */
+          }
+          
+        }
+
+       
+        @media only screen and (min-width: 601px) and (max-width: 768px) {
+          
+        }
+
+
+        @media only screen and (min-width: 769px) and (max-width: 1024px) {
+          
+        }
         
       `}</style>
       <div>
